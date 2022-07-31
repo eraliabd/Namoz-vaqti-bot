7431f38431c75a01950ae19361431f1a8f8fffe4
eraliabdinazarov <eraliabdinazarov22@gmail.com>
2022-07-31 06:47:19 +0500

    commit push

--- timer_bot.py
+++ timer_bot.py
@@ -17,7 +17,7 @@ def job():
 
 scheduler = BackgroundScheduler()
 trigger = CronTrigger(
-    year="*", month="*", day="*", hour="06", minute="45", second="50"
+    year="*", month="*", day="*", hour="06", minute="48", second="50"
 )
 scheduler.add_job(job, trigger=trigger)
 scheduler.start()
